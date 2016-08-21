0661b2f77698b3946964333018c370579ea5b9b0
Tony Bott <tonyb736@gmail.com>
2016-08-21 13:50:37 -0700

    updated home and site url to cloudfront

--- wpconfig/files/default/wp-config.php
+++ wpconfig/files/default/wp-config.php
@@ -16,8 +16,8 @@
  
 define('WP_REDIS_HOST', 'wordpress.xrkhgu.0001.use1.cache.amazonaws.com');
 
-define('WP_HOME','http://wordpress-1395981624.us-east-1.elb.amazonaws.com');
-define('WP_SITEURL','http://wordpress-1395981624.us-east-1.elb.amazonaws.com');
+define('WP_HOME','http://d2hq3za9mtqor2.cloudfront.net');
+define('WP_SITEURL','http://d2hq3za9mtqor2.cloudfront.net');
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
