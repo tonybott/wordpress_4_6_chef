af7189058b3179b839561d4d3385b975966027ea
Tony Bott <tonyb736@gmail.com>
2016-08-24 00:36:15 -0700

    Add site and home urls for demo

--- wpconfig/files/default/wp-config.php
+++ wpconfig/files/default/wp-config.php
@@ -16,8 +16,8 @@
  
 define('WP_REDIS_HOST', 'wordpress.xrkhgu.0001.use1.cache.amazonaws.com');
 
-/* define('WP_HOME','__CLOUDFRONT_DOMAIN__');
-define('WP_SITEURL','__CLOUDFRONT_DOMAIN__');
+define('WP_HOME','http://wordpress-1726082828.us-east-1.elb.amazonaws.com');
+define('WP_SITEURL','http://wordpress-1726082828.us-east-1.elb.amazonaws.com');
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
